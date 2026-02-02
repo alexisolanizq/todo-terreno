8b5066140e5d4b957c2044241f183cb92c4d679a
Alexis Olán Izquierdo <alexisolan05@icloud.com>
2026-02-02 01:02:18 -0600

    Changing files

--- src/features/catalog/components/CategoryCards.jsx
+++ src/features/catalog/components/CategoryCards.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Section from 'src/components/layout/Section'
+import tractor from 'public/tractor.webp'
+import car from 'public/car.webp'
+import truck from 'public/truck.webp'
+
 
 const CategoryCards = () => {
     return (
         <Section title="Explora Por Tipo de Llanta" description='Tenemos la llanta perfecta para cada tipo de manejo y vehículo'>
             <div className="grid lg:grid-flow-col lg:grid-rows-2 gap-4">
                 <div className='row-span-1 lg:row-span-2 relative group overflow-hidden rounded-2xl'>
-                    <img src="/public/tractor.webp" alt="Car" className='w-full lg:h-full group-hover:scale-110 transition-transform' />
+                    <img src={tractor} alt="Tractor" className='w-full lg:h-full group-hover:scale-110 transition-transform' />
                     <div className='absolute bottom-5 lg:bottom-10 left-5 lg:left-10 flex flex-col items-start gap-2'>
                         <h2 className='text-primary text-lg lg:text-3xl font-semibold'>Agrícola/Forestal</h2>
                         <p className='text-primary text-sm font-light'>Tracción fuerte para campo y bosque</p>
@@ -18,7 +22,7 @@ const CategoryCards = () => {
                     </div>
                 </div>
                 <div className='relative group overflow-hidden rounded-2xl'>
-                    <img src="/public/car.webp" alt="Car" className='w-full group-hover:scale-110 transition-transform' />
+                    <img src={car} alt="Car" className='w-full group-hover:scale-110 transition-transform' />
                     <div className='absolute bottom-5 lg:top-10 left-5 lg:right-10 flex flex-col items-start lg:items-end gap-2'>
                         <h2 className='text-primary text-3xl font-semibold'>Vehículo</h2>
                         <p className='text-primary text-sm font-light'>Seguridad y confort para tu vehículo</p>
@@ -30,7 +34,7 @@ const CategoryCards = () => {
                 </div>
 
                 <div className='relative group overflow-hidden rounded-2xl'>
-                    <img src="/public/truck.webp" alt="Car" className='w-full group-hover:scale-110 transition-transform' />
+                    <img src={truck} alt="Truck" className='w-full group-hover:scale-110 transition-transform' />
                     <div className='absolute bottom-5 lg:bottom-10 left-5 lg:left-10 flex flex-col items-start gap-2'>
                         <h2 className='text-primary text-3xl font-semibold'>Camión</h2>
                         <p className='text-primary text-sm font-light'>Resistencia y alto rendimiento en carga</p>
